3c8e31991276b0dae60ae24f400b9b102541544f
Ваше Имя <@.ru>
2026-05-26 18:34:19 +0300

    Initial commit: password generator project

---
+++ main.py
@@ -0,0 +1,5 @@
+from generator import generate_password
+
+if __name__ == "__main__":
+    pwd = generate_password()
+    print(f"Ваш пароль: {pwd}")
---
+++ utils.py
@@ -0,0 +1,5 @@
+def print_success(message):
+    print(f"[OK] {message}")
+
+def print_error(message):
+    print(f"[ERROR] {message}")
